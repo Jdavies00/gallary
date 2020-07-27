4a297073896de16f0796da03e371ab56103f3c0f
Glitch (hello-webpage) <none>
2020-07-27 20:14:38 +0000

    🚤🗽 Checkpoint ./main.js:17665856/175

--- main.js
+++ main.js
@@ -24,11 +24,15 @@ for(let i= 1; i <=5; i++){
 
 
 /* Wiring up the Darken/Lighten button */
-btn.onclick = fuintion () {
-  const bntClass= btn.getAttribute('class');
+btn.onclick = function () {
+  const btnClass= btn.getAttribute('class');
   if (btnClass === 'dark'){
     btn.setAttribute('class', 'light');
-    btn.textContent = Ligthen;
+    btn.textContent = 'Ligthen';
+    overlay.style.backgroundColor = 'rgba(0,0,0,.5)';
+  } else{
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
 }
