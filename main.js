3af63e9b530ba00f55f40f061d8ae8fc828b29fa
Glitch (hello-webpage) <none>
2020-07-29 17:14:42 +0000

    🐄🌊 Checkpoint ./main.js:17665856/34

--- main.js
+++ main.js
@@ -4,26 +4,15 @@ var thumbBar = document.querySelector('.thumb-bar');
 var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-
-
 /* Looping through images */
 for(let i= 1; i <=5; i++){
   const newImage = document.createElement('img');
   newImage.setAttribute('src', 'https://cdn.glitch.com/9d586b50-22d7-4dcf-a5a9-6127acd909c0%2Fpic' + i + '.jpg?v=1571602267425');
   thumbBar.appendChild(newImage);
-      newImage.onclick = function(e) {
-    displayedImage.src = e.target.src;
-    
+  newImage.onclick = function(e) {
+  displayedImage.src = e.target.src;   
   }
 }
-        
-
-  
-
-
-
-
-
 
 /* Wiring up the Darken/Lighten button */
 btn.onclick = function () {
